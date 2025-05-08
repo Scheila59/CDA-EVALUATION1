37e3f5f23dac3306744ec151e93ddfa2cfdf29bb
laura.flamme@hotmail.fr <laura.flamme@hotmail.fr>
2025-05-08 16:20:55 +0200

    finalisation du css de la page home + ajout de l'effet du texte a l'aide de GSAP et Lenis

--- script.js
+++ script.js
@@ -15,6 +15,12 @@ document.addEventListener("DOMContentLoaded", function () {
       loginUser();
     });
   }
+  const logoutButton = document.getElementById("btnLogout");
+  if (logoutButton) {
+    logoutButton.addEventListener("click", function () {
+      logoutUser();
+    });
+  }
 });
 
 // ---------------------Fonctionnalité register---------------------
@@ -65,3 +71,62 @@ function loginUser() {
     alert("Nom d'utilisateur ou mot de passe incorrect !");
   }
 }
+// ---------------------Fonctionnalité logout---------------------
+function logoutUser() {
+  // Supprimer les données de connexion
+  localStorage.removeItem("islogged");
+  alert("Déconnexion réussie !");
+  // Redirection vers la page de connexion
+  window.location.href = "login.html";
+}
+// fonctionnalité animation scroll text --------------------------
+
+let selection = Splitting();
+// console.log(selection);
+
+gsap.registerPlugin(ScrollTrigger);
+// selection du premier paragraphe "synopsys"---------
+
+gsap.from(selection[0].chars, {
+  y: 100,
+  rotation: 90,
+  opacity: 0,
+  stagger: 0.05,
+  scrollTrigger: {
+    trigger: ".text-reveal1", // Déclencheur : 'élément original
+    start: "top 50%", // Affiche les marqueurs de début et de fin
+    end: "bottom 50%",
+    scrub: true, // Permet de contrôler l'animation avec le scroll
+  },
+});
+// selction le deuxième paragraphe Pourquoi ?---------
+
+gsap.from(selection[1].chars, {
+  // x: 100,
+  opacity: 0,
+  stagger: 0.5, // Délai entre chaque caractère
+  scrollTrigger: {
+    trigger: ".text-reveal2", // Déclencheur : 'élément original
+    // markers: true,
+    start: "top 50%", // Affiche les marqueurs de début et de fin
+    end: "bottom 100%",
+    scrub: true, // Permet de contrôler l'animation avec le scroll
+  },
+});
+
+// -----------smooth with lenis-------------
+
+// Initialize a new Lenis instance for smooth scrolling
+const lenis = new Lenis();
+
+// Synchronize Lenis scrolling with GSAP's ScrollTrigger plugin
+lenis.on("scroll", ScrollTrigger.update);
+
+// Add Lenis's requestAnimationFrame (raf) method to GSAP's ticker
+// This ensures Lenis's smooth scroll animation updates on each GSAP tick
+gsap.ticker.add((time) => {
+  lenis.raf(time * 600); // Convert time from seconds to milliseconds
+});
+
+// Disable lag smoothing in GSAP to prevent any delay in scroll animations
+gsap.ticker.lagSmoothing(0);
